1121aaf02dfe4ec83d698b90624eecc7ea692df0
Julien <ju.gasba@gmail.com>
2016-04-06 16:36:13 +0200

    improving the code for search results

--- search.php
+++ search.php
@@ -6,12 +6,22 @@
 				<div class="post" id="post-<?php the_ID();?>">
 					<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 					<div class="postmetadata">
-					 <!-- donne des informations sur l'article : sa date , son auteur, le nombre de commentaires. Peut �tre plac� � la fin ou au d�but -->
-							<?php the_time('j F Y') ?> - <?php the_author() ?> | Cat&eacute;gorie: <?php the_category(', ') ?> |
-							<?php comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?> <?php edit_post_link('Editer', ' &#124; ', ''); ?>
+						<!-- date and author -->
+						<?php the_time('j F Y') ?> - <?php the_author_posts_link(); ?>
+						<!-- displays the 'thematiques' if it has some -->
+						<?php if (has_term( '', 'thematiques', '' )) : ?>
+							<?php the_terms( $post->ID, 'thematiques', '| <span class="dashicons dashicons-playlist-video"></span> Thématiques : ', ', ', ' ' ); ?>
+							<?php else : ?>
+						<?php endif; ?>
+						<?php if (has_term( '', 'category', '' )) : ?>
+							<?php the_terms( $post->ID, 'category', ' | Catégories : ', ', ', ' ' ); ?>
+							<?php else : ?>
+						<?php endif; ?>
+
+							| <?php comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?>
 					</div>
 					<div class="post_content">
-						<?php the_excerpt(); ?>
+						<?php the_excerpt(); ?><?php edit_post_link('Editer', '<div class="button button-primary">', '</div>'); ?>
 					</div>
 				</div>
 			<?php endwhile; ?>
