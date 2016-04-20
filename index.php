3a1c5243a76045323da8879b42a36428eb8350a9
Julien <ju.gasba@gmail.com>
2016-04-20 20:19:08 +0200

    adding if statement for video format in index.php

--- index.php
+++ index.php
@@ -3,7 +3,25 @@
 	<div class="content">
 	<?php if(have_posts()) :?>
 	<?php while(have_posts()) : the_post(); ?>
-		<div class="post" id="post-<?php the_ID();?>">
+
+		<?php if ( has_post_format( 'video' )) :?>
+
+			<div <?php post_class(); ?> id="post-<?php the_ID();?>">
+				<h2><span class="dashicons dashicons-video-alt3"></span> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+				<div class="postmetadata">
+					<!-- donne des informations sur l'article : sa date , son auteur, le nombre de commentaires. Peut �tre plac� � la fin ou au d�but -->
+					<?php the_time('j F Y') ?> - <?php the_author() ?> | Cat&eacute;gorie: <?php the_category(', ') ?> |
+					<?php comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?> <?php edit_post_link('Editer', ' &#124; ', ''); ?>
+				</div>
+				<div class="light_separator"></div>
+
+
+			</div>
+
+		<?php else : ?>
+
+
+		<div <?php post_class(); ?> id="post-<?php the_ID();?>">
 			<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 			<div class="postmetadata">
 				<!-- donne des informations sur l'article : sa date , son auteur, le nombre de commentaires. Peut �tre plac� � la fin ou au d�but -->
@@ -32,6 +50,7 @@
 			<!--<div class="post_separator"></div>--><!--s�parateur-->
 			<div class="light_separator"></div>
 		</div>
+		<?php endif; ?>
 	<?php endwhile; ?>
 		<div class="navigation">
 			<?php posts_nav_link('  ','<div class="page_next"></div>','<div class="page_prev"></div>'); ?>
@@ -46,6 +65,7 @@
 				<br/>
 			</div>
 		</div>
+
 	<?php endif; ?>
 	</div><!-- fermeture <div class="content"-->
 	<?php get_sidebar(); ?>
