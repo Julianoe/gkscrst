6f4148f1118d5f2b7fab64343352bab5841c592c
Julien <ju.gasba@gmail.com>
2018-04-08 23:48:12 +0200

    fixing opengraph tags function

--- functions.php
+++ functions.php
@@ -78,50 +78,31 @@ function geeks_curiosity_use_post_format(){
 add_action( 'after_setup_theme', 'geeks_curiosity_use_post_format' );
 
 function geeks_curiosity_opengraph() {
-    global $post;
+  global $post;
 
-    if(is_single()) {
-        if(has_post_thumbnail($post->ID)) {
-          $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'medium');
-        }
-        if($excerpt = $post->post_excerpt) {
-          $excerpt = strip_tags($post->post_excerpt);
-          $excerpt = str_replace("", "'", $excerpt);
-        } else {
-          $excerpt = get_bloginfo('description');
-        }
-        ?>
-    <meta name="description" content="<?php echo $excerpt; ?>" />
-    <meta property="og:title" content="<?php echo the_title(); ?>"/>
-    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
-    <meta property="og:type" content="article"/>
-    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
-    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
-    <meta property="og:image" content="<?php echo $img_src[0]; ?>"/>
-
-<?php
-    } else {
-
-      if(has_post_thumbnail($post->ID)) {
-        $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'large');
-      }
-      if($excerpt = $post->post_excerpt) {
-        $excerpt = strip_tags($post->post_excerpt);
-        $excerpt = str_replace("", "'", $excerpt);
-      } else {
-        $excerpt = get_bloginfo('description');
-      }
-      ?>
-    <meta name="description" content="<?php echo $excerpt; ?>" />
-    <meta property="og:title" content="<?php echo $title; ?>"/>
-    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
-    <meta property="og:type" content="article"/>
-    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
-    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
-    <meta property="og:image" content="<?php echo $img_src[0]; ?>"/>
+  if(has_post_thumbnail($post->ID)) {
+    $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'full');
+    $img_src = $img_src[0];
+  } else {
+    $img_src = '';
+  }
+  if($excerpt = $post->post_excerpt) {
+    $excerpt = strip_tags($post->post_excerpt);
+    $excerpt = str_replace("", "'", $excerpt);
+  } else {
+    $excerpt = get_bloginfo('description');
+  }
+  ?>
+  <meta name="description" content="<?php echo $excerpt; ?>" />
+  <meta property="og:title" content="<?php echo the_title(); ?>"/>
+  <meta property="og:description" content="<?php echo $excerpt; ?>"/>
+  <meta property="og:type" content="article"/>
+  <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+  <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+  <meta property="og:image" content="<?php echo $img_src; ?>"/>
 
 <?php
-    }
+  }
 }
 add_action('wp_head', 'geeks_curiosity_opengraph', 5);
 
