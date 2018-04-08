1152936a0ff750e0f91e4ed884b0b7856b139b60
Julien <ju.gasba@gmail.com>
2018-04-08 23:23:39 +0200

    Adding opengraph tags

--- functions.php
+++ functions.php
@@ -77,7 +77,55 @@ function geeks_curiosity_use_post_format(){
 }
 add_action( 'after_setup_theme', 'geeks_curiosity_use_post_format' );
 
+function geeks_curiosity_opengraph() {
+    global $post;
 
+    if(is_single()) {
+        if(has_post_thumbnail($post->ID)) {
+          $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'medium');
+        }
+        if($excerpt = $post->post_excerpt) {
+          $excerpt = strip_tags($post->post_excerpt);
+          $excerpt = str_replace("", "'", $excerpt);
+        } else {
+          $excerpt = get_bloginfo('description');
+        }
+        ?>
+    <meta name="description" content="<?php echo $excerpt; ?>" />
+    <meta property="og:title" content="<?php echo the_title(); ?>"/>
+    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+    <meta property="og:image" content="<?php echo $img_src; ?>"/>
+
+<?php
+    } else {
+
+      if(has_post_thumbnail($post->ID)) {
+        $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'medium');
+      } else {
+        $img_src = get_stylesheet_directory_uri() . '/img/opengraph_image.jpg';
+      }
+      if($excerpt = $post->post_excerpt) {
+        $excerpt = strip_tags($post->post_excerpt);
+        $excerpt = str_replace("", "'", $excerpt);
+      } else {
+        $excerpt = get_bloginfo('description');
+      }
+      ?>
+    <meta name="description" content="<?php echo $excerpt; ?>" />
+    <meta property="og:title" content="<?php echo $title; ?>"/>
+    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+    <meta property="og:image" content="<?php echo $img_src; ?>"/>
+
+<?php
+    }
+}
+add_action('wp_head', 'geeks_curiosity_opengraph', 5);
 
 
 
