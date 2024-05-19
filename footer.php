cea3db8ea3dcfff230ce86bd08d285f15e96ef03
Julien G. <ju.gasba@gmail.com>
2024-05-19 19:49:02 +0200

    add updated date copyright

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
 	<p>
-		Copyright &#169; 2016 <strong><?php echo get_bloginfo('name'); ?></strong>
+		Copyright &#169; 2016 - 2024 <strong><?php echo get_bloginfo('name'); ?></strong>
 		<br />
-		Blog propuls&eacute; par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <strong>Julianoe</strong>
+		Blog propuls&eacute; par <a href="http://wordpress.org/">WordPress</a> et rédigé et con&ccedil;u par <a href="https://julianoe.eu.org"><strong>Julianoe</strong></a>
 		<br />
 		<!--<a href="feed:<?php echo get_bloginfo('rss2_url'); ?>">Articles (RSS)</a> et <a href="feed:<?php echo get_bloginfo('comments_rss2_url'); ?>">Commentaires (RSS)</a>.-->
 		<!--<?php echo get_num_queries(); ?> requetes. <?php timer_stop(1); ?> secondes.-->
